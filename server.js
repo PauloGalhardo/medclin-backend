4b8affc7c77d7432586be66912bfd7bef7d80738
paulogalhardo <pmscgalhardo@gmail.com>
2026-03-17 13:58:31 -0300

    feat: configurando URL da API para produção_2

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
-const app = express();
-
- 
+app.use(cors()); 
 app.use(express.json()); 
 
 // Puxa a string de conexão de forma segura
